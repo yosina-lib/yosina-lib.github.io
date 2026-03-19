62e694fafbeb92bf6bc8607b84ab36eccc20560d
Moriyoshi Koizumi <mozo@mozo.jp>
2026-03-19 11:24:22 +0900

    fix: next.js deprecation warnings

--- landing-page/src/app/layout.tsx
+++ landing-page/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -16,11 +16,12 @@ export const metadata: Metadata = {
   title: "Yosina - A Japanese text normalization library you've always needed",
   description:
     "Yosina is a transliteration library that deals with the Japanese characters and symbols.",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    userScalable: false,
-  },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  userScalable: false,
 };
 
 export default async function RootLayout({
